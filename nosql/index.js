ef874a75cb9ff81b6f39de84f0f2a631705b4422
28054595 <willprobertss@gmail.com>
2019-12-06 13:48:27 +0000

    noSQL

---
+++ nosql/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded())
+
+app.get("/", (req, res) => {
+	res.sendFile('index.html')
+});
+
+app.post("/login", (req, res) => {
+	console.log(req.body);
+});
+
+mongo.connect('link', (err, db) => {
+	
+	
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	});
+});
