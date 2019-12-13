88e40939954cbfcf74d7b6b1184053a676938c61
willrushi <willprobertss@gmail.com>
2019-12-13 13:10:38 +0000

    NoSQL: Committed real version

--- nosql/index.js
+++ nosql/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
 const app = express();
+const CONNECTION_URL = "mongodb+srv://rushictf:rushictf@rushictf-v86kz.mongodb.net/test?retryWrites=true&w=majority";
+let db; 
+let collection;
 
 app.use(express.static('public'));
 app.use(express.urlencoded())
@@ -11,12 +14,45 @@ app.get("/", (req, res) => {
 
 app.post("/login", (req, res) => {
 	console.log(req.body);
+  
+  let user;
+  let password;
+  
+  try{
+    if(req.body.username.startsWith("{")){
+    user = JSON.parse(req.body.username);
+    }else{
+      user = req.body.username
+    }
+
+    if(req.body.password.startsWith("{")){
+      password = JSON.parse(req.body.password);
+    }else{
+      password = req.body.password
+    }
+    collection.find({username: user, password: password}).toArray((err, result) => {
+		if (err) res.status(500).send('Something broke!');
+    	console.log(result);
+      if(result[0]){
+        if(result[0].username === "admin"){
+          res.redirect("4573db5d49cb757ead2416ef26bd0cee.html")
+        }else{
+          res.redirect("/?loginfailed=true");
+        }
+      }else{
+          res.redirect("/?loginfailed=true");
+      }
+	  });
+  }catch(e){
+    res.status(500).send('Something broke!')
+  }
 });
 
-mongo.connect('link', (err, db) => {
-	
-	
-	app.listen(3002, function() {
-		console.log('listening on 3000');
+app.listen(3002, function() {
+	mongo.connect(CONNECTION_URL, (err, client) => {
+		if (err) return console.log(err);
+		db = client.db('rushictf');
+		collection = db.collection('users');
+		console.log('Connected to mongodb');
 	});
 });
